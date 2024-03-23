096d872e17f5c84cbea7ec54f130eda10a0f05b4
leewwwhui <leewwhui@gmail.com>
2024-03-23 22:06:24 +1100

    ✨ feat: update readme

--- README.md
+++ README.md
@@ -35,3 +35,6 @@ The easiest way to deploy your Next.js app is to use the [Vercel Platform](https
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
 # nextjs-template
+![dark](https://raw.githubusercontent.com/leewwhui/imagestore/master/nextjs-template-light.png)
+
+![dark](https://raw.githubusercontent.com/leewwhui/imagestore/master/nextjs-template-dark.png)
--- src/components/layout/navigation/navigation.tsx
+++ src/components/layout/navigation/navigation.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { MainNav } from "./main-nav";
 import { Search } from "@/components/search";
 import { MobileNav } from "./mobile-nav";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import { ThemeButton } from "@/components/buttons/theme-button";
 
 export const Navigation = () => {
@@ -14,13 +12,6 @@ export const Navigation = () => {
 
       <div className="flex items-center gap-3">
         <Search />
-
-        <Link href="/signin">
-          <Button variant="ghost" size="sm">
-            Sign in
-          </Button>
-        </Link>
-
         <ThemeButton />
       </div>
     </nav>
